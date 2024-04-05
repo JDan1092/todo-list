7eb163c2a51b12a15976239ae1962f5a58e7ad46
JDan1092 <95508276+JDan1092@users.noreply.github.com>
2024-04-05 16:57:22 -0600

    change the code to make it work with the other files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log('testing index.js webpack. Is it working? Yes it is');
+import { createToDo } from './create-to-do.js';
+import { blankProjectLoad } from './blank-project-load.js';
+import { displayDefaultProject, displayTheForm, addItemToChecklist, clearForm } from './dom-manip.js';
+import './style.css';
+
+blankProjectLoad();
+
+// initialdomManip();
+
+let clickEventModule = (function() {
+
+    const addNewToDo = document.querySelector(".add-todo-button");
+    addNewToDo.addEventListener("click", displayTheForm);
+
+    const addToCheckList = document.querySelector(".add-to-checklist");
+    addToCheckList.addEventListener("click", addItemToChecklist);
+
+    const clearButton = document.querySelector(".reset-button");
+    clearButton.addEventListener("click", clearForm);
+})
+
+const myToDo = createToDo("Grocery Run", "Go get groceries", "6/15/2024", "low, meat, eggs, milk");
+const myToDo2 = createToDo("Homework", "Do Odin Project", "6/16/2024", "medium", "Read modules, do todo project");
+console.log("Show me properties on myToDo from index.js.....", myToDo);
+console.log("Show me properties on my ToDo2 from index.js.....", myToDo2);
