2eea36d7da6e37596b3c6a083023d53693e0248f
JDan1092 <95508276+JDan1092@users.noreply.github.com>
2024-04-10 16:58:05 -0600

    add the date-fns function and code

--- src/create-to-do.js
+++ src/create-to-do.js
@@ -1,11 +1,44 @@
+import { compareAsc, format, parseISO, startOfToday } from 'date-fns';
+import { clearForm } from './dom-manip';
+
 let toDoArray = [];
 
-export const createToDo = (Title, Description, DueDate, Priority, Checklist) => {
+export const createToDo = () => {
+
+    let Title = document.getElementById("Title").value;
+    let Description = document.getElementById("Description").value;
+    let DueDate = document.getElementById("DueDate").value;
+    let Priority = document.getElementById("Priority").value;
+
+    if (Title == "" || Description == "" || DueDate == "") {
+        alert("Title, Description and Due Date are required fields, please try again!");
+        return;
+    }
+
+    if (parseISO(DueDate) < startOfToday()) {
+        alert("You have entered a date that has already passed! Please enter a date greater that or equal to today.");
+        console.log("due date", parseISO(DueDate));
+        console.log("due date", startOfToday());
+        return;
+    }
+
+    const nodeListCheckList = document.querySelectorAll("li");
+    let CheckListArray = [];
+    for (let i = 0; i < nodeListCheckList.length; i++) {
+        let strippedCheckList = nodeListCheckList[i].textContent.replace("\u00D7", '');
+        CheckListArray.push(strippedCheckList);
+    }
+
+    let CheckList = CheckListArray.join(", ");
+
     console.log('Called createToDo module...creating todo now');
-    console.log({ Title, Description, DueDate, Priority, Checklist });
+    console.log({ Title, Description, DueDate, Priority, CheckList });
     console.log('Pushing this object to the toDoArray....');
     
-    toDoArray.push({ Title, Description, DueDate, Priority, Checklist });
+    toDoArray.push({ Title, Description, DueDate, Priority, CheckList });
     console.log(toDoArray);
-    return { Title, Description, DueDate, Priority, Checklist};
+
+    clearForm();
+
+    return { Title, Description, DueDate, Priority, CheckList};
 }
